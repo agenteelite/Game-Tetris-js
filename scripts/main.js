a11089ffecd074a2c8915d1eb48ddc101dfc0a49
agenteelite <agenteelite63@gmail.com>
2026-01-25 09:44:46 -0500

    Enhance game logic with score and menu controls
    Added score display and game over menu functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 
-import { Game } from "./game.js";
+import { Game } from '/scripts/game.js';
 
 const canvasTetris = document.getElementById("canvas-tetris");
 const canvasNext = document.getElementById("canvas-next");
+const score = document.getElementById("score");
+const menu = document.getElementById("menu");
+const btnmenu = document.getElementById("btn-start");
+
 const rows = 20;
 const columns = 10;
 const cellSize = 26;
@@ -11,14 +15,26 @@ const space = 2;
 const game = new Game(canvasTetris, rows, columns, cellSize, space, canvasNext);
 
 function update() {
-
-    game.update();
-
+    if(game.gameOver){
+        menu.style.display = "flex";
+    } else {
+        game.update();
+        score.innerHTML = game.score;
+    }
+    
     requestAnimationFrame(update);
 
 }
-
+//Evento para iniciar el juego desde el menu
+btnmenu.addEventListener("click", () => {
+    setTimeout(() => {
+        menu.style.display = "none";
+        game.reset(); 
+    }, 200);
+    
+});
 update();
+
 
 
 
